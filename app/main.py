c9c4ea9dcc73c5a6d973932dfcc50008d6b62119
Vova <vovakucin082@gmail.com>
2025-10-22 11:42:17 +0200

    corrections are made

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ def format_linter_error(error: dict) -> dict:
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
-        "errors": [format_linter_error(fail_eror) for fail_eror in errors],
+        "errors": [format_linter_error(error) for error in errors],
         "path": file_path,
         "status": "failed" if errors else "passed"
     }
@@ -18,6 +18,6 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 def format_linter_report(linter_report: dict) -> list:
     return [
-        format_single_linter_file(errors, linter_report[errors])
-        for errors in linter_report
+        format_single_linter_file(file_path, errors)
+        for file_path, errors in linter_report.items()
     ]
